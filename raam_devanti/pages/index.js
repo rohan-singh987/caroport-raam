179b233a0487b9f63f31aa886bc167ed4e6d843a
AKASH SINGH <furiousakash111@gmail.com>
2022-04-04 13:08:16 +0530

    Home page done

--- raam_devanti/pages/index.js
+++ raam_devanti/pages/index.js
@@ -10,12 +10,12 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className='flex justify-end'>
-        <span className='bg-blue-600 align-middle p-2 text-white font-medium rounded-bl-xl'>I'm a Doctor</span>
+        <span className='bg-blue-600 align-middle p-2 text-white font-medium rounded-bl-xl lg:text-lg'>I'm a Doctor</span>
       </div>
-      <div className='p-10'>
-        <div className='bg-slate-400 py-5 rounded-xl shadow-2xl'>
+      <div className='p-10 sm:px-28 md:py-14 md:px-40 lg:py-20 lg:px-56 xl:py-24 xl:px-72'>
+        <div className='bg-slate-400 py-5 rounded-xl shadow-2xl sm:px-3'>
           <div className='w-4/5 mx-auto flex mb-3'>
-            <div className='w-2/5'>
+            <div className='w-3/5'>
               <Image
                 src="/media/icon1.png"
                 height="750"
@@ -23,16 +23,22 @@ export default function Home() {
                 alt="Caroport icon"
               />
             </div>
-            <div className='w-3/5 my-auto'>
-              <span className='uppercase font-semibold text-lg'>Caroport</span>
-            </div>
+              <span className='uppercase font-semibold text-lg my-auto lg:text-2xl'>Caroport</span>
           </div>
-          <div className='bg-slate-700 w-4/5 p-5 mx-auto mb-5 rounded-lg'>
+          <div className='bg-slate-700 w-4/5 p-5 mx-auto mb-5 rounded-lg sm:mb-8 lg:w-3/5'>
             <form action="">
-              <label htmlFor="uname" className='text-white'>Username:</label>
-              <input className='mt-2 p-1 mb-2 rounded-lg' type="text" id='uname' name='uname' placeholder='username' />
-              <label htmlFor="pass" className='text-white'>Password:</label>
-              <input type="password" id='pass' name='pass' placeholder='password' className='mt-2 p-1 mb-5 rounded-lg' />
+              <div className='sm:w-1/3'>
+                <label htmlFor="uname" className='text-white'>Username:</label>
+              </div>
+              <div className='mt-2 sm:w-1/3 mx-auto mb-2 lg:mb-5'>
+                <input className='p-1 rounded-lg' type="text" id='uname' name='uname' placeholder='username' />
+              </div>
+              <div className='sm:w-1/3'>
+                <label htmlFor="pass" className='text-white'>Password:</label>
+              </div>
+              <div className='mt-2 sm:w-1/3 mx-auto mb-5 sm:mb-8 lg:mb-10'>
+                <input type="password" id='pass' name='pass' placeholder='password' className='p-1 rounded-lg' />
+              </div>
               <div className='flex justify-center'>
                 <button type='submit' className='text-white rounded-lg bg-blue-800 p-2 font-semibold'>Log In</button>
               </div>
@@ -40,7 +46,7 @@ export default function Home() {
           </div>
           <div className='flex justify-around'>
             <span className='align-middle bg-blue-800 p-2 rounded-lg text-white font-semibold'>Sign Up</span>
-            <span className='text-sky-800 font-medium text-sm align-middle my-auto'>Forgot Password?</span>
+            <span className='text-sky-800 font-medium text-sm align-middle my-auto md:text-base'>Forgot Password?</span>
           </div>
         </div>
       </div>
